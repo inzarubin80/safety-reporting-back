751f7e4f3eaab68ef21ecb7b2f78fa219668f40f
Ivan <inzarubin80@gmail.com>
2021-01-21 22:05:09 +0300

    hardData

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const hardData = [{ user: 'a', duration:1, role:user }, { user: 'b', duration:1, role:admin }];
+
+app.get('/users', function (req, res) {
+  
+  res.jsonp(hardData);
+
+}
+)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
